a643533e5be4ecb2f4fd1dbbaafb6bcef5031c42
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-20 17:34:12 -0600

    working on schema

--- data/migrations/20190920172227_prof_schema.js
+++ data/migrations/20190920172227_prof_schema.js
@@ -1,5 +1,13 @@
 
 exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+      tbl.increments()
+
+      tbl.string('username', 128).unique().notNullable()
+      tbl.string('password', 128).notNullable()
+      tbl.string('first_name', 128).notNullable()
+      tbl.string('last_name', 128).notNullable()
+  })
   
 };
 
