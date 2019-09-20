29e7feefe640310575fa9e148aef57094a2769dc
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-20 17:14:18 -0600

    created skeleton userrouter

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
-
 const server = express()
+const UserRouter = require('../users/user-router')
+
+server.use('/users', UserRouter)
 
 server.get('/', (req, res) => {
     res.send('Is this thing on?')
