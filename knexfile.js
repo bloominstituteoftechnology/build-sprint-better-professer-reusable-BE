9c9feae63662c7c3804dd1fe005966735e127936
laura <lauradondiego@gmail.com>
2019-09-23 16:25:05 -0400

    added env file

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,42 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './data/users.db3'
+      filename: "./data/users.db3"
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds"
     },
     pool: {
       afterCreate: (conn, done) => {
         // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       }
-    },
+    }
   },
-
-
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/users.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
+    }
+  }
 };
