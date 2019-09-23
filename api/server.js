babe1d509a329c402275c09203de29f56ce24eb9
laura <lauradondiego@gmail.com>
2019-09-23 16:04:39 -0400

    added cors dependency

--- api/server.js
+++ api/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const server = express();
 const UserRouter = require("../users/user-router");
-const StudentsRouter = require("../students/students-router");
-const MessagesRouter = require("../messages/messages-router");
-const ProjectsRouter = require("../projects/projects-router");
+// const StudentsRouter = require("../students/students-router");
+// const MessagesRouter = require("../messages/messages-router");
+// const ProjectsRouter = require("../projects/projects-router");
 
 server.use(express.json());
+server.use(cors());
 
 server.use("/users", UserRouter);
-server.use("/students", StudentsRouter); // get, add, delete, put
-server.use("/messages", MessagesRouter); // get, add
-server.use("/projects", ProjectsRouter); // get, add, delete, put
+// server.use("/students", StudentsRouter); // get, add, delete, put
+// server.use("/messages", MessagesRouter); // get, add
+// server.use("/projects", ProjectsRouter); // get, add, delete, put
 
 server.get("/", (req, res) => {
   res.send("Is this thing on?");
