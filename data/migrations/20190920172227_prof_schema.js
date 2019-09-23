ad615e5c2358d8b2e4c26ecd58e1898506287a2d
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-23 11:32:49 -0600

    finished schema

--- data/migrations/20190920172227_prof_schema.js
+++ data/migrations/20190920172227_prof_schema.js
@@ -10,11 +10,55 @@ exports.up = function(knex) {
   })
   .createTable('students', tbl => {
       tbl.increments()
-       
       
+      tbl.string('student_name', 128).notNullable()
+      tbl.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+  })
+  .createTable('projects', tbl => {
+      tbl.increments()
+
+      tbl.string('project_name', 128).notNullable()
+      tbl.string('description', 128)
+      tbl.integer('student_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('students')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+  })
+  .createTable('messages', tbl => {
+      tbl.increments()
+
+      tbl.text('message').notNullable()
+      tbl.datetime('date_time').notNullable()
+      tbl.integer('student_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('students')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      tbl.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
   })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('messages')
+  .dropTableIfExists('projects')
+  .dropTableIfExists('students')
+  .dropTableIfExists('users')
 };
