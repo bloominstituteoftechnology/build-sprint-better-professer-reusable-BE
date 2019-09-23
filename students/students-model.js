70c40a9601ce9a91841b191ebf0f89b8e163b3c0
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-23 15:55:07 -0600

    added skeleton students-model

---
+++ students/students-model.js
@@ -0,0 +1,30 @@
+const db = require('../data/dbConfig')
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return null
+}
+
+function findById(id) {
+    return null
+}
+
+function add() {
+    returremove
+}
+
+function update() {
+    return null
+}
+
+function remove() {
+    return null
+}
+
