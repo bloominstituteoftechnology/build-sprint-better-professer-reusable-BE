3e26129f265fc84d386e1bc3cb001a8d6283d6fe
laura <lauradondiego@gmail.com>
2019-09-24 11:57:41 -0400

    adjusted endpoints in projects router

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,15 +1,8 @@
 const router = requre("express").Router();
 const Users = require("../users/user-model");
 
-router.get("/students/projects", (req, res) => {
-  Users.find()
-    .then(allProjects => {
-      res.json({ allProjects });
-    })
-    .catch(err => res.send(err));
-});
 
-router.get("/students/:id/projects", (req, res) => {
+router.get("/students/:id", (req, res) => {
   const { id } = req.params;
   Users.find(id)
     .then(projects => {
@@ -18,7 +11,7 @@ router.get("/students/:id/projects", (req, res) => {
       } else {
         res
           .status(404)
-          .json({ messgae: "Could not find projects for given student" });
+          .json({ message: "Could not find projects for given student" });
       }
     })
     .catch(err => {
@@ -26,7 +19,7 @@ router.get("/students/:id/projects", (req, res) => {
     });
 });
 
-router.post("/students/projects", (req, res) => {
+router.post("/", (req, res) => {
   const newProject = req.body;
   Users.add(newProject)
     .then(project => {
@@ -76,10 +69,10 @@ router.delete("/:id", (req, res) => {
 
 module.exports = router;
 
-// {project_name: "Snow Flair", deadline: "11/1/2019", description: "The sky is clear,the stars are twinkling.", student_id: 1},
-// {project_name: "Chive Agenda", deadline: "12/12/2019", description: "Joe made the sugar cookies, Susan decorated them", student_id: 1},
-// {project_name: "Flip Pick", deadline: "2/1/2020", description: "The book is in front of the table", student_id: 1},
-// {project_name: "Peak Row", deadline: "11/25/2019", description: "The mysterious diary records the voice", student_id: 2},
-// {project_name: "Jive Rex", deadline: "12/11/2019", description: "The waves were crashing on the shore, it was a lovely sight", student_id: 2},
-// {project_name: "Flip Veil", deadline: "12/3/2019", description: "We need to rent a room for our party", student_id: 3},
-// {project_name: "Bay Hound", deadline: "1/19/2020", description: "It was getting dark, and we weren’t there yet", student_id: 3}
+// {project_name: "Draw a Blank", deadline: "11/1/2019", description: "The sky is clear,the stars are twinkling.", student_id: 1},
+// {project_name: "Wild Goose Chase", deadline: "12/12/2019", description: "Futilely pursue something that will never be attainable", student_id: 1},
+// {project_name: "Raining Cats and Dogs", deadline: "2/1/2020", description: "Make it rain very heavily", student_id: 1},
+// {project_name: "Shot In the Dark", deadline: "11/25/2019", description: "Attempt something that has little chance for success", student_id: 2},
+// {project_name: "Jumping the Gun", deadline: "12/11/2019", description: "Start too early before preparations are ready", student_id: 2},
+// {project_name: "Tough it Out", deadline: "12/3/2019", description: "Remain resillient even in hard times.", student_id: 3},
+// {project_name: "Elephant in the room", deadline: "1/19/2020", description: "Ignore a large, obvious problem or fail to address an issue that stands out in a major way", student_id: 3}
