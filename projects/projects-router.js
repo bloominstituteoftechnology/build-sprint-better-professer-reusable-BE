d38f8d6b25e91891c702fdedc6bff49674962bd6
laura <lauradondiego@gmail.com>
2019-09-24 15:55:45 -0400

    completed projects endpoints and tested

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -32,22 +32,21 @@ router.post("/", (req, res) => {
 router.put("/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
-      if (project) {
+  
         projectsmodel.update(id, changes).then(updatedProject => {
+          if(updatedProject) {
           res.json(updatedProject);
-        })
-            .catch(err => {
-      res.status(500).json({ message: "Failed to update project" });
-   
-    })
-
-      } else {
+          } else {
         res
           .status(404)
           .json({ message: "Could not find project with given ID" });
       }
-    })
-   
+      })
+            .catch(err => {
+              console.log(err)
+      res.status(500).json({ message: "Failed to update project" });
+      })
+})
 
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
@@ -67,11 +66,3 @@ router.delete("/:id", (req, res) => {
 });
 
 module.exports = router;
-
-// {project_name: "Draw a Blank", deadline: "11/1/2019", description: "The sky is clear,the stars are twinkling.", student_id: 1},
-// {project_name: "Wild Goose Chase", deadline: "12/12/2019", description: "Futilely pursue something that will never be attainable", student_id: 1},
-// {project_name: "Raining Cats and Dogs", deadline: "2/1/2020", description: "Make it rain very heavily", student_id: 1},
-// {project_name: "Shot In the Dark", deadline: "11/25/2019", description: "Attempt something that has little chance for success", student_id: 2},
-// {project_name: "Jumping the Gun", deadline: "12/11/2019", description: "Start too early before preparations are ready", student_id: 2},
-// {project_name: "Tough it Out", deadline: "12/3/2019", description: "Remain resillient even in hard times.", student_id: 3},
-// {project_name: "Elephant in the room", deadline: "1/19/2020", description: "Ignore a large, obvious problem or fail to address an issue that stands out in a major way", student_id: 3}
