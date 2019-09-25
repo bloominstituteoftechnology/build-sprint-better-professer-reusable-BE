6094964085205214564ae79375657d85c330a0f8
laura <lauradondiego@gmail.com>
2019-09-25 11:33:20 -0400

    merging recent changes

--- api/server.js
+++ api/server.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const server = express();
-const restrict = require('../auth/restricted-middleware')
-const UserRouter = require("../users/user-router");
-const StudentsRouter = require("../students/students-router");
-const MessagesRouter = require("../messages/messages-router");
-const ProjectsRouter = require("../projects/projects-router");
+const restrict = require('../auth/restricted-middleware');
+const UserRouter = require('../users/user-router');
+const StudentsRouter = require('../students/students-router');
+const MessagesRouter = require('../messages/messages-router');
+const ProjectsRouter = require('../projects/projects-router');
 
 server.use(express.json());
 server.use(cors());
 
-server.use("/users", UserRouter);
-server.use("/students", restrict, StudentsRouter);
-server.use("/messages", MessagesRouter);
-server.use("/projects", ProjectsRouter);
+server.use('/users', UserRouter);
+server.use('/students', restrict, StudentsRouter);
+server.use('/messages', restrict, MessagesRouter);
+server.use('/projects', restrict, ProjectsRouter);
 
-server.get("/", (req, res) => {
-  res.send("Is this thing on?");
+server.get('/', (req, res) => {
+	res.send('Is this thing on?');
 });
 
 module.exports = server;
--- students/students-router.js
+++ students/students-router.js
@@ -1,17 +1,17 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-const Students = require('./students-model')
+const Students = require('./students-model');
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params 
-    Students.find(id)
-        .then(students => {
-            res.status(200).json(students)
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).json({ error: 'Could not load students'})
-        })
-})
+	const { id } = req.params;
+	Students.find(id)
+		.then((students) => {
+			res.status(200).json(students);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ error: 'Could not load students' });
+		});
+});
 
-module.exports = router
+module.exports = router;
